472ff9ebeea2162816c02485afc01870d44e1be6
Chris Burke <cdburke@gmail.com>
2019-06-02 17:34:44 -0700

    initial commit of repos and install script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# addonrepos
-list of addon repos
+This repo has the list of addon repos for the current and beta systems.
+
+The install.sh script will install them all in linux and macos.
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# make addons subdirectory and install all addons
+
+cd `dirname "$(realpath $0)"`
+
+# protocol
+# P=https|git
+P=git
+
+mkdir addons
+cd addons
+git init .
+
+f() {
+ p=`sed "s/github:/github.com\//" <<<"$1"`
+ d=`sed "s/_/\//" <<<"${1##*/}"`
+ git submodule add $P://$p $d
+}
+
+cat ../repos.txt | while read p; do
+ f $p
+done
