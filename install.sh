d72af391b5b4e172820ed523154fb673b68a3286
bill lam <bbill.lam@gmail.com>
2023-04-08 15:04:54 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # make addons subdirectory and install all addons
 
@@ -8,7 +8,13 @@ cd `dirname "$(realpath $0)"`
 # P=https|git
 # use git instead of https if you have write access to the repos
 
+# url = git@github.com:jsoftware/arc_zip.git
+# url = https://github.com/jsoftware/jsource.git
+
+set -e
+
 P=git
+# P=https
 
 mkdir addons
 cd addons
@@ -16,9 +22,16 @@ git init .
 
 f() {
  n=`sed "s/|.*//" <<< "$1"`
- p=`sed "s/github:/github.com\//" <<<"$n"`
- d=`sed "s/_/\//" <<<"${n##*/}"`
- git submodule add $P://$p $d
+ if [ "$P" = "git" ] ; then
+  p=`sed "s/github:/github.com:/" <<<"$n"`
+  d=`sed "s/_/\//" <<<"${n##*/}"`
+  git submodule add $P@$p $d
+ else
+  p=`sed "s/github:/github.com\//" <<<"$n"`
+  d=`sed "s/_/\//" <<<"${n##*/}"`
+  git submodule add $P://$p $d
+ fi
+
 }
 
 cat ../repos.txt | while read p; do
